09c373932bff02cac2637391372e588dc2d82782
Nick Quaranto <nick@quaran.to>
2013-11-27 18:28:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Switchboard::Application.config.secret_key_base = '6f9aaab2f0e8c89d3c4367b38dc9ddc7a92f5b20d38049a71b8fbede4f0f4ea417d8cffeb818d7d581871d0e50fb5b8d0e8244386fd4b2a1ac0676848fe4ebb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Switchboard::Application.config.session_store :cookie_store, key: '_switchboard_session'
