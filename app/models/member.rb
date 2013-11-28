ecde606479a0301edff6176125a08880e2dde38b
Nick Quaranto <nick@quaran.to>
2013-11-27 20:35:11 -0500

    Validate presence of phone number

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ActiveRecord::Base
   phony_normalize :phone_number, default_country_code: 'US'
-  validates :phone_number, phony_plausible: true
+  validates :phone_number, phony_plausible: true, presence: true
   validates :name, presence: true
 
   def self.find_from_number!(number)
