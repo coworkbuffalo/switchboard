ee6d545ff4b6269fdf5aed355907a46172ea86a2
Nick Quaranto <nick@quaran.to>
2013-12-03 21:11:02 -0500

    Add highlight on current nav link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link_to(name, path)
+    options = {}
+    options[:class] = "active" if controller.class.name.include?(name)
+
+    link_to name, path, options
+  end
 end
