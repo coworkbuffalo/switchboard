79a4340da84a8d391413a5d4986beed293f19ec3
Nick Quaranto <nick@quaran.to>
2014-02-04 16:55:55 -0500

    Bump sleep to 2

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -38,7 +38,7 @@ class Entry < ActiveRecord::Base
 
   def notify!
     RestClient.post "http://#{ENV["DOOR_USER"]}:#{ENV["DOOR_TOKEN"]}@#{ENV["DOOR_URL"]}/api/device/CoworkBuffalo", switch: "on"
-    sleep 1
+    sleep 2
     RestClient.post "http://#{ENV["DOOR_USER"]}:#{ENV["DOOR_TOKEN"]}@#{ENV["DOOR_URL"]}/api/device/CoworkBuffalo", switch: "off"
   end
 end
