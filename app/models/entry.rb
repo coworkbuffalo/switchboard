96b7bea53686c428470abb3c098b31c80fe827a9
Nick Quaranto <nick@quaran.to>
2014-08-13 09:41:51 -0400

    Try new blink API

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -37,8 +37,6 @@ class Entry < ActiveRecord::Base
   end
 
   def notify!
-    RestClient.post "http://#{ENV["DOOR_USER"]}:#{ENV["DOOR_TOKEN"]}@#{ENV["DOOR_URL"]}/api/device/CoworkBuffalo", state: "on"
-    sleep 3
-    RestClient.post "http://#{ENV["DOOR_USER"]}:#{ENV["DOOR_TOKEN"]}@#{ENV["DOOR_URL"]}/api/device/CoworkBuffalo", state: "off"
+    RestClient.post "http://#{ENV["DOOR_USER"]}:#{ENV["DOOR_TOKEN"]}@#{ENV["DOOR_URL"]}/api/device/CoworkBuffalo", state: "blink", delay: 4
   end
 end
